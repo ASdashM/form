ec32f69a73055b1951c713b2a727b54843708f15
ASdashM <putyatindanil30@gmail.com>
2023-09-27 20:28:57 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useForm} from 'react-hook-form'
+import "./styles.css"
 
-function App() {
+
+const App = () => {
+
+  const {
+    register,
+    formState: {
+      errors, isValid
+    },
+    handleSubmit,
+    reset
+
+  } = useForm({
+    mode: "onBlur"
+  })
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+    reset();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="app">
+      <h1>React Hook Form</h1>
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          First Name:
+          <input
+            {...register('firstName', {
+              required: "Поле обязательно к заполнению",
+              minLength: {
+                value: 5,
+                message: "Минимум 5 символов"
+              }
+            })
+          }
+          />
+        </label>
+        <label>
+          Last Name:
+          <input
+            {...register('firstName', {
+              required: "Поле обязательно к заполнению",
+              minLength: {
+                value: 5,
+                message: "Минимум 5 символов"
+              }
+            })
+          }
+          />
+        </label>
+        <div style = {{height: 20}}>
+          {errors?.firstName && <p>{errors?.firsName?.message || ""}</p>}
+        </div>
+        <input type="submit" disabled={!isValid}/>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
